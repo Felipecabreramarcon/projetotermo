9e090e16dc1fad8c5863da4e426e638c004bf4cb
Felipe marcon <felipemarcon357@gmail.com>
2024-04-15 12:47:03 -0300

    Logica ajustada

--- script.js
+++ script.js
@@ -1,92 +1,78 @@
-let cont = 0;
-let tentativa = "";
-let letras = document.getElementsByClassName("td");
-let teste = 5;
-let linha = 1;
-let del = document.getElementById('delete');
-function colocaLetra(event) {
-    let palavra = "CARAI";
-
-    if (cont <= teste) {
-        if (cont < 29) {
-            letras[cont + 1].style.outline = "2px solid black"
-        } else {
-            letras[cont].style.outline = "2px solid black"
-        }
-        let temp = event.target.innerHTML;
-        letras[cont].innerHTML = temp;
-        tentativa += temp;
-        cont++;
-        console.log(cont);
-        letras[cont - 1].style.outline = "none";
-
+let teclas = document.querySelectorAll(".tecla")
+let linhas = document.querySelectorAll("tr")
+let enterButton = document.querySelector(".ENTER")
+let deleteButton = document.querySelector("#delete")
+let linhaAtual = 0
+let tentativa = ''
+cont = 1
+let palavra = "macaco"
+function digita(tecla) {
+    if (cont <= palavra.length) {
+        tentativa += tecla
+        linhas[linhaAtual].childNodes[cont].innerHTML = tecla
+        atual()
+        cont++
     }
-    if (cont % 5 == 0) {
-        letras[cont].style.outline = "2px solid black"
-    }
-    if (cont >= teste) {
-        linha += 1;
-        for (let i = teste - 5, j = 0; i < teste; i++, j++) {
-            let cor = "";
-            if (tentativa[j] === palavra[j]) {
-                cor = "#3aa394"; // letra correta na posição correta
-            } else if (palavra.includes(tentativa[j])) {
-                cor = "#d3ad69"; // letra correta, mas na posição errada
-            } else {
-                cor = ""; // letra errada
+}
+deleteButton.addEventListener("click", backspace)
+function backspace() {
+    linhas[linhaAtual].childNodes[cont - 1].innerHTML = ""
+    cont--
+    tentativa = tentativa.slice(0, -1)
+    console.log(tentativa)
+    atual()
+}
+function enter() {
+    if (tentativa.length < palavra.length) {
+        alert("erro")
+    } else {
+        palavra = palavra.toUpperCase()
+        for (let i = 0; i < palavra.length; i++) {
+            console.log(tentativa[i], palavra[i])
+            if (tentativa[i] === palavra[i]) {
+                if (linhas[linhaAtual].childNodes[i + 1]) {
+                    linhas[linhaAtual].childNodes[i + 1].style.backgroundColor = "#3aa394"; // Letra correta na posição correta
+                }
+            } else if (palavra.includes(tentativa[i])) {
+                if (linhas[linhaAtual].childNodes[i + 1]) {
+                    linhas[linhaAtual].childNodes[i + 1].style.backgroundColor = "#d3ad69"; // Letra correta, mas na posição errada
+                }
             }
-            letras[i].style.backgroundColor = cor;
         }
-        tentativa = "";
-        teste += 5;
+        linhas[linhaAtual].childNodes[cont - 1].style.border = "none"
+        tentativa = ""
+        linhaAtual++
+        cont = 1
+        atual()
     }
-}
+    if (tentativa === palavra) {
+        linhas[linhaAtual].childNodes.forEach(e => {
+            e.style.backgroundColor = "#3aa394"
+        })
+    }
 
-let teclas = document.getElementsByClassName("tecla");
-for (let i = 0; i < teclas.length; i++) {
-    teclas[i].addEventListener("click", colocaLetra);
 }
-for (let i = 0; i < letras.length; i++) {
-    letras[i].addEventListener("click", () => seleciona(i));
-}
-function seleciona(i) {
+enterButton.addEventListener("click", enter)
+function start(palavra) {
 
-    let limiteInferior = (linha - 1) * 5;
-    let limiteSuperior = linha * 5;
-
-    if (i >= limiteInferior && i < limiteSuperior) {
-        for (let j = 0; j < letras.length; j++) {
-            letras[j].style.outline = 'none';
+    linhas.forEach((linha) => {
+        for (let i = 0; i < palavra.length; i++) {
+            const letra = document.createElement('td');
+            letra.classList = "td";
+            linha.appendChild(letra);
         }
-
-        letras[i].style.outline = '2px solid black';
-        cont = i;
-    }
+    });
 }
 
+teclas.forEach((tecla) => {
+    tecla.addEventListener("click", () => digita(tecla.innerHTML))
+})
 
-
-
-
-
-del.addEventListener("click", voltatecla);
-
-function voltatecla() {
-    console.log(cont);
-
-
-    letras[cont].innerHTML = '';
-    if (cont > 0 && cont % 5 !== 0) {
-
-        tentativa.
-            console.log(tentativa);
-        atual = cont;
-
-        letras[cont - 1].style.outline = "2px solid black";
-        letras[cont].style.outline = "none"
-        cont--;
+function atual() {
+    for (let i = 0; i < palavra.length; i++) {
+        linhas[linhaAtual].childNodes[i + 1].style.border = "none"
     }
-
-
+    linhas[linhaAtual].childNodes[cont].style.border = "1px solid black"
 }
 
+start(palavra)
